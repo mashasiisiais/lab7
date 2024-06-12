0424519723589eb625753bc90897839542158acd
mashasiisiais <152235219+mashasiisiais@users.noreply.github.com>
2024-06-12 17:24:52 +0300

    Add project files.

---
+++ lab7/Abonent.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace laba04
+{
+    public class Abonent: IComparable
+    {
+        public string Name { get; set; }
+        public string Number { get; set; }
+        public string Info ()
+        {
+            return $"{Name}, {Number}, {Service}, {Extra}, {CostT}, {Debt}, {Contract}";
+        }
+        public int CompareTo (object obj)
+        {
+            Abonent t = obj as Abonent;
+            return string.Compare(this.Name, t.Name);
+        }
+        public double Service { get; set; }
+        public double Extra { get; set; }
+        public double CostT { get; set; }
+        
+        public bool Debt { get; set; }
+        public bool Contract { get; set; }
+        public double YearIncome()
+        {
+
+            return (Service * CostT) + Extra;
+        }
+        public Abonent()
+        { 
+        }
+        public Abonent(string name, string number, 
+                 double service, double extra, double costT, bool debt, bool contract)
+        {
+            Name = name;
+            Number = number;
+            Debt = debt;
+            Contract = contract;
+            Service = service;
+            Extra = extra;
+            CostT = costT;
+
+        }
+    }
+   
+}
---
+++ lab7/Program.cs
@@ -0,0 +1,74 @@
+﻿using laba04;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab7
+{
+
+    internal class Program
+    {
+        static List<Abonent> abonents;
+
+        static void PrintAbonents()
+        {
+            foreach (Abonent abonent in abonents)
+            {
+                Console.WriteLine(abonent.Info());
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            abonents = new List<Abonent>();
+            try
+            {
+                using (FileStream fs = new FileStream("FINALY.abonents", FileMode.Open))
+                using (BinaryReader br = new BinaryReader(fs))
+                {
+                    while (br.BaseStream.Position < br.BaseStream.Length)
+                    {
+                        Abonent abonent = new Abonent();
+                        abonent.Name = br.ReadString();
+                        abonent.Number = br.ReadString();
+                        abonent.Extra = br.ReadDouble();
+                        abonent.CostT = br.ReadDouble();
+                        abonent.Service = br.ReadDouble();
+                        abonent.Debt = br.ReadBoolean();
+                        abonent.Contract = br.ReadBoolean();
+
+                        abonents.Add(abonent);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Сталась помилка: \n{0}", ex.Message);
+            }
+
+            Console.WriteLine("Несортований перелік абонентів: {0}", abonents.Count);
+            PrintAbonents();
+            abonents.Sort();
+            Console.WriteLine("Сортований перелік абонентів: {0}", abonents.Count);
+            PrintAbonents();
+            Console.WriteLine();
+            Abonent abonetOleg = new Abonent("Семенко Олег Григорович", "+3742839", 34, 40, 30, true, false);
+            abonents.Add(abonetOleg);
+            abonents.Sort();
+            Console.WriteLine("Перелік абонентів: {0}", abonents.Count);
+            PrintAbonents();
+            Console.WriteLine("Видаляємо останнє значення");
+            abonents.RemoveAt(abonents.Count - 1);
+            Console.WriteLine("Перелік абонентів {0}", abonents.Count);
+            PrintAbonents();
+            Console.ReadKey();
+        }
+    }
+
+
+}
